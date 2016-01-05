a5ce193d7ded0ca331c8a0940582a8e50a27966c
Jiang Sheng <gisonrg@gmail.com>
2016-01-06 00:51:14 +0800

    Add map to deliver view.

--- www/js/controllers/deliverController.js
+++ www/js/controllers/deliverController.js
@@ -7,6 +7,61 @@
  * # DeliverController
  */
 angular.module('MyApp')
-  .controller('DeliverController', function($scope) {
+  .controller('DeliverController', function($scope, $ionicLoading, socket) {
+    $scope.markers = [];
+    // Adds a marker to the map and push to the array.
+    function addMarker(location) {
+      var marker = new google.maps.Marker({
+        position: location,
+        map: $scope.map
+      });
 
+      // Add info window
+      var infoWindow = new google.maps.InfoWindow({
+        content: 'This is a new marker',
+        noSupress: true
+      });
+
+      marker.addListener('click', function() {
+        infoWindow.open($scope.map, marker);
+      });
+      $scope.markers.push(marker);
+    }
+
+    $scope.initMap = function(map) {
+      $scope.map = map;
+
+      if (!$scope.map) {
+        return;
+      }
+
+      $scope.loading = $ionicLoading.show({
+        template: 'Getting current location'
+      });
+
+      navigator.geolocation.getCurrentPosition(function(pos) {
+        var myLocation = new google.maps.LatLng(pos.coords.latitude,
+          pos.coords.longitude);
+        $scope.map.setCenter(myLocation);
+        $scope.map.setZoom(14);
+
+        addMarker(myLocation);
+
+        var LatLng = {
+          lat: pos.coords.latitude,
+          lng: pos.coords.longitude
+        };
+
+        $ionicLoading.hide();
+      }, function(error) {
+        console.log('Unable to get location: ' + error.message);
+        $ionicLoading.hide();
+      });
+    };
+
+
+
+    socket.on('connect', function() {
+      console.log('connected');
+    });
   });
--- www/js/services/SocketService.js
+++ www/js/services/SocketService.js
@@ -8,21 +8,21 @@
  */
 angular.module('MyApp')
   // use factory for services
-  .factory('socket', function ($rootScope) {
-    var socket = io.connect('localhost:3000');
+  .factory('socket', function($rootScope, ApiService) {
+    var socket = io.connect(ApiService.getEndpoint());
     return {
-      on: function (eventName, callback) {
-        socket.on(eventName, function () {
+      on: function(eventName, callback) {
+        socket.on(eventName, function() {
           var args = arguments;
-          $rootScope.$apply(function () {
+          $rootScope.$apply(function() {
             callback.apply(socket, args);
           });
         });
       },
-      emit: function (eventName, data, callback) {
-        socket.emit(eventName, data, function () {
+      emit: function(eventName, data, callback) {
+        socket.emit(eventName, data, function() {
           var args = arguments;
-          $rootScope.$apply(function () {
+          $rootScope.$apply(function() {
             if (callback) {
               callback.apply(socket, args);
             }
